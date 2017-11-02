98f2ddbf2abdd052f51b1482b981718dd7f2e7b3
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-11-01 22:31:11 -0700

    handle paginated batch requests

--- util.py
+++ util.py
@@ -4,19 +4,37 @@ import boto3
 def get_all_job_info():
     "get a list of all jobs on all queues in all states"
     batch = boto3.client('batch')
-    # FIXME handle pagination (nextToken)
-    queues = batch.describe_job_queues()['jobQueues']
+    queues = do_paginated_batch_operation(batch,
+                                          "describe_job_queues",
+                                          'jobQueues')
     states = ['SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING',
               'RUNNING', 'FAILED', 'SUCCEEDED']
     jobinfo = []
+    # TODO sort by queue name?
     for queue in queues:
+        qname = queue['jobQueueName']
         if queue['state'] == 'DISABLED' or not queue['status'] == 'VALID':
             continue
         queueinfo = {}
         for state in states:
-            # FIXME handle paginated results (nextToken)
-            queueinfo[state] = batch.list_jobs(jobQueue=queue['jobQueueName'],
-                                               jobStatus=state)['jobSummaryList']
-        jobinfo.append(dict(queue=queue['jobQueueName'],
-                            info=queueinfo))
+            queueinfo[state] = do_paginated_batch_operation(batch, "list_jobs",
+                                                            'jobSummaryList',
+                                                            dict(jobQueue=qname,
+                                                                 jobStatus=state))
+        jobinfo.append(dict(queue_name=qname,
+                            info=queueinfo,
+                            queue=queue))
     return jobinfo
+
+def do_paginated_batch_operation(batch, operation, wanted_part, args=None):
+    "do paginated batch operation"
+    if args is None:
+        args = {}
+    out = []
+    while True:
+        result = getattr(batch, operation)(**args)
+        out.extend(result[wanted_part])
+        if 'nextToken' in result:
+            args['nextToken'] = result['nextToken']
+        else:
+            return out
