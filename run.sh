78d4be389338f8c2ef965d1ee9392414c3023309
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-04-15 12:30:26 -0700

    attempt to stop baking secrets into the docker image

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 
+APP_SECRET=$(cat /run/secrets/BATCH_DASHBOARD_APP_SECRET)
+export APP_SECRET
+AWS_ACCESS_KEY_ID=$(cat /run/secrets/BATCH_DASHBOARD_AWS_ACCESS_KEY_ID)
+export AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=$(cat /run/secrets/BATCH_DASHBOARD_AWS_SECRET_ACCESS_KEY)
+export AWS_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION=$(cat /run/secrets/BATCH_DASHBOARD_AWS_DEFAULT_REGION)
+export AWS_DEFAULT_REGION
+
 VENV=$(pipenv --venv)
 
-$VENV/bin/gunicorn app_namespace:APP --timeout 120 -w 4 -b 0.0.0.0:8001
+"$VENV/bin/gunicorn app_namespace:APP" --timeout 120 -w 4 -b 0.0.0.0:8001
